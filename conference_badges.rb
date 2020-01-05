a5c3c6e5fcdd7a7a920f55155df7fd4552c4a0c0
Erin Stansbury <ekstansbury@gmail.com>
2020-01-05 13:40:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker(name)
+  namebadge = "Hello, my name is #{name}."
+  return namebadge
+end
+
+def batch_badge_creator(attendees)
+  my_array = []
+  attendees.each do |att|
+    my_array.push("Hello, my name is #{att}.")
+  end
+  return my_array
+end
+
+def assign_rooms(attendees)
+  room_array = []
+  room_num = 1
+  attendees.each do |attend|
+    room_array.push("Hello, #{attend}! You'll be assigned to room #{room_num}!")
+    room_num += 1
+  end
+  return room_array
+end
+
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
+end
